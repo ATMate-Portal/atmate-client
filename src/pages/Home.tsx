0deb40bea180d50097d28ebfa29469c14da2ba8e
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-03-09 15:19:04 +0000

    MElhorado design dos accordions da página principal
    Adicionar headers
    Adicionar icones de urgênica
    Adicionar data mais próxima de pagamento no accordion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,23 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Home.css";
 
 const Home: React.FC = () => {
+
   return (
+    <div className="container"> {/* Adicionei um container para centralizar */}
     <div className="accordion-container">
       <div className="accordion w-100 mx-auto" id="mainAccordion">
+        
         {/* Primeiro Item */}
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
             <button
-              className="accordion-button"
+              className="accordion-button fs-4 d-flex justify-content-between align-items-center"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
               aria-expanded="true"
               aria-controls="collapseOne"
             >
-              CTT
+              <div className="d-flex align-items-center w-100 justify-content-between"> {/* Envolvendo tudo em um div */}
+                <div className="d-flex align-items-center">
+                  <span className="fs-4">🚨</span>
+                  <span className="ms-4 fs-3">Softinsa</span>
+                </div>
+                <span className="text-muted me-5">4 dias até expirar</span>
+              </div>
             </button>
           </h2>
+          
           <div
             id="collapseOne"
             className="accordion-collapse collapse show"
@@ -31,25 +41,34 @@ const Home: React.FC = () => {
             </div>
           </div>
         </div>
-
+        {/* FIM Primeiro Item */}
         {/* Segundo Item */}
         <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
+          <h2 className="accordion-header" id="headingOne">
             <button
-              className="accordion-button collapsed"
+              className="accordion-button fs-4"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseTwo"
-              aria-expanded="false"
+              aria-expanded="true"
               aria-controls="collapseTwo"
             >
-              CRITICAL
+              
+
+              <div className="d-flex align-items-center w-100 justify-content-between"> {/* Envolvendo tudo em um div */}
+                <div className="d-flex align-items-center">
+                  <span className="fs-4">⚠️</span>
+                  <span className="ms-4 fs-3">NTT Data</span>
+                </div>
+                <span className="text-muted me-5">10 dias até expirar</span>
+              </div>
             </button>
           </h2>
+          
           <div
             id="collapseTwo"
-            className="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
             data-bs-parent="#mainAccordion"
           >
             <div className="accordion-body">
@@ -57,7 +76,9 @@ const Home: React.FC = () => {
             </div>
           </div>
         </div>
+        {/* FIM Segundo Item */}
       </div>
+    </div>
     </div>
   );
 };
@@ -67,6 +88,12 @@ const TableComponent: React.FC = () => {
     <div className="table-responsive">
       <table className="table custom-table">
         <tbody>
+        <tr className="table custom-header">
+          <td>Imposto</td>
+          <td>Montante</td>
+          <td>Data limite</td>
+          <td></td>
+        </tr>
           <tr>
             <td>IRS</td>
             <td>500€</td>
